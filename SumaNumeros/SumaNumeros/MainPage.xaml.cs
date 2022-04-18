fc03fb753330c97c8e724638639a736bbdb563d9
Tonypradog <60700823+Tonypradog@users.noreply.github.com>
2022-04-18 13:25:00 -0500

    chore Todo

---
+++ SumaNumeros/SumaNumeros/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SumaNumeros
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                float a = float.Parse(valor1.Text);
+                float b = float.Parse(valor2.Text);
+                float c = a + b;
+                resultado.Text = a + "+" + b + "=" + c;
+
+            }
+            catch (Exception)
+            {
+                resultado.Text = "Datos Invalidos";
+            }
+        }
+    }
+}
